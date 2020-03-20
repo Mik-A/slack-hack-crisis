0c8f986744901ef553eb4cc6d54cd45a04a97e16
Mik-A <mika.valimaki@gmail.com>
2020-03-20 12:12:02 +0200

    env

--- src/lambda/getChannels.js
+++ src/lambda/getChannels.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 export async function handler(event, context) {
-  const token =
-    'xoxp-998425034449-1011950159975-1014308500070-52bf485d951b0deec9303c6316d1ba77' // process.env.NODE_ENV_TOKEN
+  const token = process.env.NODE_ENV_TOKEN
   try {
     const list = await axios.get(
       `https://slack.com/api/conversations.list?token=${token}&pretty=1`
